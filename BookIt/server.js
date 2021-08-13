013bd9e6fc4935afddae5512bc70fb8ea7e0b98f
Nolan Hovis <nolan.hovispersonal@gmail.com>
2021-08-13 12:36:09 -0500

    Server Path Change

--- BookIt/server.js
+++ BookIt/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/book-it'));
+app.use(express.static('./dist/BookIt'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/book-it/'}),
+    res.sendFile('index.html', {root: 'dist/BookIt/'}),
 );
 
 // Start the app by listening on the default Heroku port
